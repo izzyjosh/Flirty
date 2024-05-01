c3914ce248818030d7a71eb0785617b47e9810ea
izzyjosh <calebayuba222@gmail.com>
2024-05-01 02:13:30 +0100

    update for deployment

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",".onrender.com"]
 
 
 # Application definition
